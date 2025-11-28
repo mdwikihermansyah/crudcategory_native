e211b31bb6ead1f3e7b1fb97902e0318c5c409f2
Nama Kamu <dwikihermansyah17@gmail.com>
2025-11-28 14:36:58 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"go-web-native/config"
+	"go-web-native/controllers/categorycontroller"
+	"go-web-native/controllers/homecontroller"
+
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ConnectDB()
+
+	//1. homepage
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	//2. categories
+	http.HandleFunc("/categories", categorycontroller.Index)
+
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
